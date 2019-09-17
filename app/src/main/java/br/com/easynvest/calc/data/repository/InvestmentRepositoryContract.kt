f350384a56406ca1246006f2263eddba0e971ccf
Roger Silva <p000roger.martins@prservicos.com.br>
2019-09-17 11:52:44 -0300

    Remoção de imports

--- app/src/main/java/br/com/easynvest/calc/data/repository/InvestmentRepositoryContract.kt
+++ app/src/main/java/br/com/easynvest/calc/data/repository/InvestmentRepositoryContract.kt
@@ -1,6 +1,5 @@
 package br.com.easynvest.calc.data.repository
 
-import br.com.easynvest.calc.data.model.SimulateResponse
 import br.com.easynvest.calc.data.model.SimulateRequest
 import br.com.easynvest.calc.entity.BaseResult
 import io.reactivex.Single
