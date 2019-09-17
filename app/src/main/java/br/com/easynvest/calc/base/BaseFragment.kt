c4daadb704a6e7d660b1d073e4cb9c052f1d086b
Roger Silva <p000roger.martins@prservicos.com.br>
2019-09-17 08:41:18 -0300

    Adicionado metodos para alterar a visibilidade de um componente.

--- app/src/main/java/br/com/easynvest/calc/base/BaseFragment.kt
+++ app/src/main/java/br/com/easynvest/calc/base/BaseFragment.kt
@@ -1,13 +1,28 @@
 package br.com.easynvest.calc.base
 
+import android.view.View
 import androidx.fragment.app.Fragment
+import io.reactivex.disposables.CompositeDisposable
 
 open class BaseFragment<T> : Fragment() {
+
+    val compositeDisposable: CompositeDisposable by lazy {
+        CompositeDisposable()
+    }
 
     var listener: T? = null
 
     override fun onDetach() {
+        compositeDisposable.clear()
         super.onDetach()
         listener = null
+    }
+
+    fun setVisibilityVisible(view: View) {
+        view.visibility = View.VISIBLE
+    }
+
+    fun setVisibilityGone(view: View) {
+        view.visibility = View.GONE
     }
 }
