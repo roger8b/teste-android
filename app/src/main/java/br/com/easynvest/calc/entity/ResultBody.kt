0ce419396cdefc9abcd13fde4afac5ad9d9adcf6
Roger Silva <p000roger.martins@prservicos.com.br>
2019-09-17 11:35:43 -0300

    Alterado tipo do parametro name para InvestmentField

--- app/src/main/java/br/com/easynvest/calc/entity/ResultBody.kt
+++ app/src/main/java/br/com/easynvest/calc/entity/ResultBody.kt
@@ -1,6 +1,8 @@
 package br.com.easynvest.calc.entity
 
+import br.com.easynvest.calc.data.model.InvestmentField
+
 data class ResultBody(
-    val name: String,
+    val name: InvestmentField,
     val value: String
 ) : BaseResult
